86f5a42f62836cdc15a5fde3f5908a4b6446a071
justinhwbhang <117044443+justinhwbhang@users.noreply.github.com>
2023-02-06 01:09:18 -0800

    ORM

---
+++ routes/api/category-routes.js
@@ -0,0 +1,55 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+router.get('/', (req, res) => {
+    Category.findAll({
+        include: [Product],
+
+    }).then(allCategories => res.json(allCategories))
+
+        .catch(err => res.status(400).json(err))
+
+});
+
+router.get('/:id', (req, res) => {
+    Category.findOne({
+        where: {
+            id: req.params.id
+        },
+
+        include: [Product],
+
+    }).then(singleCategory => res.json(singleCategory))
+
+        .catch(err => res.status(400).json(err))
+});
+
+router.post('/', (req, res) => {
+    Category.create(req.body)
+
+        .then(createdCategory => res.json(createdCategory))
+
+        .catch(err => res.status(400).json(err))
+});
+
+router.put('/:id', (req, res) => {
+    Category.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    }).then(updatedCategory => res.json(updatedCategory))
+
+        .catch(err => res.status(400).json(err))
+});
+
+router.delete('/:id', (req, res) => {
+    Category.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(deletedCategory => res.json(deletedCategory))
+
+        .catch(err => res.status(400).json(err))
+});
+
+module.exports = router;
---
+++ routes/api/tag-routes.js
@@ -0,0 +1,64 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+router.get('/', (req, res) => {
+    Tag.findAll({
+        include: [
+            {
+                model: Product,
+                through: ProductTag
+            }
+        ],
+
+    }).then(allTags => res.json(allTags))
+
+        .catch(err => res.status(400).json(err))
+});
+
+router.get('/:id', (req, res) => {
+    Tag.findOne({
+        where: {
+            id: req.params.id
+        },
+
+        include: [
+            {
+                model: Product,
+                through: ProductTag
+            }
+        ],
+
+    }).then(singleTag => res.json(singleTag))
+
+        .catch(err => res.status(400).json(err))
+});
+
+router.post('/', (req, res) => {
+    Tag.create(req.body)
+
+        .then(createdTag => res.json(createdTag))
+
+        .catch(err => res.status(400).json(err))
+});
+
+router.put('/:id', (req, res) => {
+    Tag.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    }).then(updatedTag => res.json(updatedTag))
+
+        .catch(err => res.status(400).json(err))
+});
+
+router.delete('/:id', (req, res) => {
+    Tag.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(deletedTag => res.json(deletedTag))
+
+        .catch(err => res.status(400).json(err))
+});
+
+module.exports = router;
